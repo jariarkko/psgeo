25a9a3ff48141e63afdfbb01cad7d433b662baee
Ralf Strandell <5670717+ralfstrandell@users.noreply.github.com>
2020-09-10 11:06:21 +0100

    Cleanup of work files

--- lib/create-translation-script-se.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# ONLY WORKS FOR SINGLE LINE DEFINITON
-# THE REST MUST BE EDITED MANUALLY IN CODE
-
-grep '^[ ]*addTranslation(".*, fi:' psgeolang.js | sed -e 's/^.*, fi: "/"/' -e 's/ \});[ ]*$//' | sed 's/.*/&\/&, se: "" \});/' | sed 's/^/s\//' > sed-commands.sed
-
